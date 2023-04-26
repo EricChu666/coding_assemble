86ff5f3cd2fce669af2ad9298d9463732f8b4ef7
Eric Chu <ericchu0826@gmail.com>
2023-04-26 13:28:10 +0800

    Quess number 2

--- Quess_random_number_1_100.py
+++ Quess_random_number_1_100.py
@@ -1,8 +1,10 @@
 import random
 
 r = random.randint(1, 100)
+count = 0
 
 while True:
+	count += 1
 	num = input('please quess a number(1-100):')
 	num = int(num)
 	if num == r:
@@ -13,3 +15,4 @@ while True:
 		print('Your number is bigger than the answer.')
 	elif num < r:
 		print('Your number is smaller than the answer.')
+	print('This is the,', count, 'times you quess')
