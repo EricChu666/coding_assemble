312f53835beb298db98796188f50e1458afaab51
Eric Chu <ericchu0826@gmail.com>
2023-04-26 22:57:21 +0800

    Files reading commit2

--- reviews_analysis.py
+++ reviews_analysis.py
@@ -1,10 +1,13 @@
 data = []
 count = 0
+sum_len = 0
 
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 10000 == 0:
-			print(len(data))
-print(data[0])
+print('Files analysis is complete, the total length is:', len(data))
+
+for d in data:
+	sum_len = sum_len + len(d)
+print('The average length of the data is:', sum_len/len(data))
