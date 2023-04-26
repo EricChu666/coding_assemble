d129bba52fa9ceb76dbbb9ced64debe597de1279
Eric Chu <ericchu0826@gmail.com>
2023-04-26 23:14:31 +0800

    filter data which is under 100 chars

--- reviews_analysis.py
+++ reviews_analysis.py
@@ -1,6 +1,7 @@
 data = []
 count = 0
 sum_len = 0
+new = []
 
 with open('reviews.txt', 'r') as f:
 	for line in f:
@@ -9,5 +10,10 @@ with open('reviews.txt', 'r') as f:
 print('Files analysis is complete, the total length is:', len(data))
 
 for d in data:
+	if len(d) < 100:
+		new.append(d)
 	sum_len = sum_len + len(d)
+
 print('The average length of the data is:', sum_len/len(data))
+print('There are', len(new), 'datas which length is under 100.')
+print(new[1])
