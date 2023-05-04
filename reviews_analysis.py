bab14dffb6fafe2a61ac1913a3d6682b3846698f
Eric Chu <ericchu0826@gmail.com>
2023-05-04 15:46:31 +0800

    add words count function

--- reviews_analysis.py
+++ reviews_analysis.py
@@ -2,12 +2,7 @@ data = []
 count = 0
 sum_len = 0
 new = []
-
-with open('reviews.txt', 'r') as f:
-	for line in f:
-		data.append(line)
-		count += 1
-print('Files analysis is complete, the total length is:', len(data))
+wc = {}	#word_count
 
 for d in data:
 	if len(d) < 100:
@@ -22,6 +17,43 @@ for d in data:
 	if 'good' in d:
 		good.append(d)
 
-#faster typing: good = [d for d in date if good in d]
+faster typing: good   =   [d   for   d	 in   date   if good in d]
+#			   output	 運算	   變數		 清單	篩選條件
 
 print('There are', len(good), 'datas include the word "good."')
+
+bad = ['bad' in d for d in data]
+print(bad)
+bad = []
+for d in data:
+	bad.append('bad' in d)
+
+#文字計數
+
+with open('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+		count += 1
+	print('Files analysis is complete, the total length is:', len(data))
+
+	for d in data:
+		words = d.strip().split()
+		for word in words:
+			if word in wc:
+				wc[word] += 1
+			else:
+				wc[word] = 1
+
+
+while True:
+	word = input('請輸入你想要查找的字: ')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word, '的出現次數為:', wc[word], '次')
+	else:
+		print('這個字沒有出現過喔!')
+print('感謝你使用查找功能。')
+
+
+
